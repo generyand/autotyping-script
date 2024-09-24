d5669b1b6b6faf3b91655f51df1712fa690ce05c
generyand <generyan.dep@gmail.com>
2024-09-24 20:55:43 +0800

    phrase count changed from 40 to 35

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@
 EDGE_PATH = "C:/Program Files (x86)/Microsoft/Edge/Application/msedge.exe"
 BING_URL = "https://www.bing.com"
 INITIAL_DELAY = 2
-PHRASE_COUNT = 40
+PHRASE_COUNT = 35
 TYPING_DELAY = 6
